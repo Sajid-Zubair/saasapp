4d3ce941006e96829d00040b50184542a397b1dd
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-18 15:08:23 -0400

    implement clerk signin and signout

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import NavItems from './NavItems'
+import { SignInButton, SignedIn, SignedOut, UserButton} from '@clerk/nextjs';
 const Navbar = () => {
   return (
     <nav className='navbar'>
@@ -18,7 +19,16 @@ const Navbar = () => {
       </Link>
       <div className='flex items-center gap-8'>
             <NavItems />
-            <p>Sign In</p>
+            <SignedOut>
+              <div className='flex items-center gap-2'>
+                <SignInButton>
+                  <button className='btn-signin'>Sign In</button>
+                </SignInButton>
+              </div>
+            </SignedOut>
+            <SignedIn>
+              <UserButton afterSignOutUrl = "/"/>
+            </SignedIn>
       </div>
     </nav>
   )

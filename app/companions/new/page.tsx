43f6c29d4f0938e64ae638a58e7a87da03120865
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-19 22:45:35 -0400

    implement companions limit for users

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,18 +1,37 @@
 import CompanionForm from '@/components/ui/CompanionForm'
 import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs/server';
-
+import { newCompanionPermissions } from '@/lib/actions/companion.action';
+import Image from 'next/image';
+import Link from 'next/link';
 const NewCompanion = async () => {
   const { userId } = await auth();
   
   if(!userId) redirect('/sign-in');
 
+  const canCreateCompanion = await newCompanionPermissions();
+
+
+
   return (
     <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+      {canCreateCompanion ? (
       <article className='w-full gap-4 flex flex-col '>
         <h1>Companion Builder</h1>
         <CompanionForm />
       </article>
+      ) : (
+        <article className='companion-limit'>
+          <Image src="/images/limit.svg" alt="Limit" width={360} height={230} />
+          <div className='cta-badge'>
+            Upgrade your plan
+          </div>
+          <h1>You have reached your companion limit</h1>
+          <p>Upgrade your plan to create more companions and access premium features</p>
+          <Link href="/subscription" className='btn-primary w-full justify-center'>Upgrade Now</Link>
+        </article>
+      )
+    }
     </main>
   )
 }
